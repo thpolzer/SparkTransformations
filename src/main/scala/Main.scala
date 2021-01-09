96bb3d7845b6c616c8050d2a2efa12883c8b619f
thpolzer <tpolzer69@gmail.com>
2021-01-09 10:52:57 +0100

    case class added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,13 +3,20 @@ import org.apache.spark.sql.SparkSession
 import sparkconnection.SparkConfiguration
 
 object Main extends App {
- // System.setProperty("HADOOP_USER_NAME","hdpuser")
+
   val sc = new SparkConfiguration(args(0),args(1)).sparkconfig._1
  // print(sc.sparkUser)
 
   val rddFromFile = sc.textFile("hdfs://hadoopserver:9000/hdpuser/inputdata/inputdata_timeseries.csv")
 
- // val rddFromFile1 = rddFromFile.collect()
+  val rddFromFile1 = rddFromFile.collect()
+
+
+
+
+
+
+
 
   val rdd = sc.parallelize(List(1,2,3,4,5,6)).cache()
   val t1= rdd.map(x => x*2)
---
+++ src/main/scala/recordtypes/Measures.scala
@@ -0,0 +1,3 @@
+package recordtypes
+
+case class Measures(direction:Int, station:String, calyear:Int, day:String, allocation: Double, capa: Double)
