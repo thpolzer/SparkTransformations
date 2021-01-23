2be26e520f69a6dc1c988ef6a19230933698b282
thpolzer <tpolzer69@gmail.com>
2021-01-23 13:52:39 +0100

    mysql connection

--- build.sbt
+++ build.sbt
@@ -7,3 +7,7 @@ scalaVersion := "2.11.12"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.7"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.7"
+
+// https://mvnrepository.com/artifact/mysql/mysql-connector-java
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.16"
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,16 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SparkSession
+import java.sql.DriverManager
+
 import sparkconnection.SparkConfiguration
 import recordtypes.Measures
 
 object Main extends App {
 
-  val sc = new SparkConfiguration(args(0),args(1)).sparkconfig._1
+  val sparkcontext = new SparkConfiguration(args(0),args(1))
+  val sc = sparkcontext.sparkconfig._1
+  val sqlcontext = sparkcontext.sparkconfig._2
   val rddFromFile = sc.textFile("hdfs://hadoopserver:9000/hdpuser/inputdata/inputdata_timeseries.csv").filter(row => !(row.split(",")(0).equals("\"TYPE\"")))
   val schemaInferredRDD = rddFromFile
       .map(_.split(","))
@@ -22,5 +27,22 @@ object Main extends App {
     .map (mydata => ((mydata.station,mydata.calyear),mydata.allocation))
     .reduceByKey((x,y) => (x+y)/2)
     .sortByKey()
-    .saveAsTextFile("hdfs://hadoopserver:9000/hdpuser/output/aggregatedAllocations.txt")
+ //   .saveAsTextFile("hdfs://hadoopserver:9000/hdpuser/output/aggregatedAllocations.txt")
+
+  import sqlcontext.implicits._
+  val jdbcDF = aggregateRDD.toDF()
+
+  val dataframe_mysql = sqlcontext
+    .read.format("jdbc")
+    .option("url", "jdbc:mysql://hadoopserver:3306/testuser?characterEncoding=utf8&useSSL=false&useUnicode=true")
+    .option("driver", "com.mysql.jdbc.Driver")
+    .option("dbtable", "mytable")
+    .option("user", "hiveuser")
+    .option("password", "09.Rudi.Voeller").load()
+  dataframe_mysql.show()
+
+  val a = 1
+
 }
+
+
